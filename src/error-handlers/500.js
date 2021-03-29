dba7c547892a97888eb4ec24a9f9051dac94e2a7
BayanAbualhaj <bayanabualhaj1097@gmail.com>
2021-03-29 17:55:21 +0300

    dev complete

---
+++ src/error-handlers/500.js
@@ -0,0 +1,10 @@
+  
+'use strict';
+
+module.exports = (err, req, res, next) => {
+  res.status(500);
+  res.statusMessage = '500/Server Error :(';
+  res.json({
+    error: 'SERVER ERROR =(',
+  });
+};
